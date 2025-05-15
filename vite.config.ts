2c13e27b8c98aa716356909a48584707db386c41
Logan Isham <loganisham@Logans-Laptop.local>
2025-05-14 23:30:25 -0600

    Prepare for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/betting-betting-buddies-info/',
   server: {
     host: "::",
     port: 8080,
